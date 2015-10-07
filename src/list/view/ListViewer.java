18c9f981e17dec840975504a3d0983c0122b6460
Adam Kinzer <adamkinzer915@gmail.com>
2015-10-07 13:55:26 -0600

    Added lists

---
+++ src/list/view/ListViewer.java
@@ -0,0 +1,11 @@
+package list.view;
+
+import javax.swing.JOptionPane;
+
+public class ListViewer
+{
+	public void showList(String currentWords)
+	{
+		JOptionPane.showMessageDialog(null, currentWords);
+	}
+}
